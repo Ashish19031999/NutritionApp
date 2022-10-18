6ee07e1d2cc7349eaba0ba267f45bbd9bcde87d0
Ashish19031999 <ashishyadav19031999@gmail.com>
2022-10-18 20:34:05 +0530

    UPDATED

--- src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/exception/UserApiRequestException.java
+++ src/main/java/com/group_4_trial_1/Nutri_App_user_Trial/exception/UserApiRequestException.java
@@ -1,7 +1,12 @@
 package com.group_4_trial_1.Nutri_App_user_Trial.exception;
 
 public class UserApiRequestException extends RuntimeException{
-    public UserApiRequestException(String message, Throwable cause) {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UserApiRequestException(String message, Throwable cause) {
         super(message, cause);
     }
 
